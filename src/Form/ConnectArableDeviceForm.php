12561cec45cac0f71665475436ce0cf3f7de67e7
paul121 <paul.weidner@gmail.com>
2021-05-06 13:47:01 -0700

    Optionally assign the sensor location when connecting to farmOS.

--- src/Form/ConnectArableDeviceForm.php
+++ src/Form/ConnectArableDeviceForm.php
@@ -4,10 +4,12 @@ namespace Drupal\farm_arable\Form;
 
 use Drupal\asset\Entity\Asset;
 use Drupal\Component\Serialization\Json;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\data_stream\Entity\DataStream;
 use Drupal\farm_arable\ArableClientInterface;
+use Drupal\log\Entity\Log;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
@@ -16,6 +18,15 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
 class ConnectArableDeviceForm extends FormBase {
 
   /**
+   * The entity type manager.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  protected $entityTypeManager;
+
+  /**
+   * The arable client service.
+   *
    * @var ArableClientInterface $arableClient
    */
   protected $arableClient;
@@ -23,10 +34,13 @@ class ConnectArableDeviceForm extends FormBase {
   /**
    * Constructs the ConnectArableDeviceForm.
    *
+   * @param EntityTypeManagerInterface $entity_type_manager
+   *   The entity type manager service.
    * @param \Drupal\farm_arable\ArableClientInterface $arable_client
    *   The Arable client service.
    */
-  public function __construct(ArableClientInterface $arable_client) {
+  public function __construct(EntityTypeManagerInterface $entity_type_manager, ArableClientInterface $arable_client) {
+    $this->entityTypeManager = $entity_type_manager;
     $this->arableClient = $arable_client;
   }
 
@@ -35,6 +49,7 @@ class ConnectArableDeviceForm extends FormBase {
    */
   public static function create(ContainerInterface $container) {
     return new static(
+      $container->get('entity_type.manager'),
       $container->get('farm_arable.arable_client'),
     );
   }
@@ -168,6 +183,78 @@ class ConnectArableDeviceForm extends FormBase {
       $form['device_info'][$form_key]['#value'] = $response[$response_key];
     }
 
+    // Display device location info.
+    // @todo Display location on the map. Meta data in popup.
+    $form['device_info']['location'] = [
+      '#type' => 'details',
+      '#title' => $this->t('Device location'),
+      '#open' => TRUE,
+    ];
+
+    // Current location name.
+    $current_location = $response['current_location']['name'] ?? '';
+    $form['device_info']['location']['name'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Current location Name'),
+      '#default_value' => $current_location,
+      '#disabled' => TRUE,
+    ];
+
+    // Current location GPS.
+    $gps = $response['current_location']['gps'] ?? [];
+    $form['device_info']['location']['gps'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('GPS'),
+      '#default_value' => join(',', $gps),
+      '#disabled' => TRUE,
+    ];
+
+    // Checkbox to assign location.
+    // @todo Should it be possible to make the Arable sensor "fixed"?
+    $form['device_info']['location']['assign_location'] = [
+      '#type' => 'checkbox',
+      '#title' => $this->t('Assign location'),
+      '#description' => $this->t('Assign the Arable sensor location when creating the device.'),
+      '#default_value' => TRUE,
+    ];
+
+    $form['device_info']['location']['use_gps'] = [
+      '#type' => 'checkbox',
+      '#title' => $this->t('Use GPS'),
+      '#description' => $this->t("Use the reported GPS value as the sensor's location?"),
+      '#default_value' => TRUE,
+      '#states' => [
+        'visible' => [
+          ':input[name="device_info[location][assign_location]"]' => ['checked' => TRUE],
+          ':input[name="device_info[location][gps]"]' => ['filled' => TRUE],
+        ],
+      ],
+    ];
+
+    // Optionally specify a location asset as the location.
+    $form['device_info']['location']['asset_location_reference'] = [
+      '#type' => 'entity_autocomplete',
+      '#title' => $this->t('Location'),
+      '#target_type' => 'asset',
+      '#selection_handler' => 'views',
+      '#selection_settings' => [
+        'view' => [
+          'view_name' => 'farm_location_reference',
+          'display_name' => 'entity_reference',
+        ],
+        'match_operator' => 'CONTAINS',
+        'match_limit' => 10,
+      ],
+      '#tags' => TRUE,
+      '#validate_reference' => FALSE,
+      '#maxlength' => 1024,
+      '#states' => [
+        'visible' => [
+          ':input[name="device_info[location][assign_location]"]' => ['checked' => TRUE],
+        ],
+      ],
+    ];
+
     // @todo Display device "has_bridge".
     // @todo Display device sensors.
 
@@ -221,6 +308,39 @@ class ConnectArableDeviceForm extends FormBase {
       'data_stream' => $data_stream,
     ]);
     $sensor->save();
+
+    // Assign the sensor location.
+    if (!empty($device_info['location']) && !empty($device_info['location']['assign_location'])) {
+
+      // Start a movement log.
+      $movement_log = Log::create([
+        'type' => 'activity',
+        'status' => 'done',
+        'name' => $this->t('Assign Arable device location.'),
+        'is_movement' => TRUE,
+        'asset' => $sensor,
+      ]);
+
+      // Specify the geometry.
+      if (!empty($device_info['location']['gps'] && !empty($device_info['location']['use_gps']))) {
+        $gps = explode(',', $device_info['location']['gps']);
+        $geom = "POINT ($gps[0] $gps[1])";
+        $movement_log->set('geometry', $geom);
+      }
+
+      // Specify the location.
+      if (!empty($device_info['location']['asset_location_reference'])) {
+        $locations = [];
+        $location_ids = array_column($device_info['location']['asset_location_reference'], 'target_id');
+        if (!empty($location_ids)) {
+          $locations = $this->entityTypeManager->getStorage('asset')->loadMultiple($location_ids);
+        }
+        $movement_log->set('location', $locations);
+      }
+
+      // Save the movement log.
+      $movement_log->save();
+    }
 
     // Display message.
     $this->messenger()->addMessage($this->t('Created sensor: <a href=":asset_uri">%asset_label</a>', [
